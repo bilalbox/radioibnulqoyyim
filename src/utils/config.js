a078a42099dca8823930df9207825e7725529a48
bilalbox <bilalbox@gmail.com>
2020-11-05 10:01:11 +0800

    removed Radio IQ Gowa

--- src/utils/config.js
+++ src/utils/config.js
@@ -9,26 +9,20 @@ const cfg = {
       {
         title: 'RADIO IQ - SALURAN 1',
         audioInfo:
-          'https://cors-anywhere.herokuapp.com/https://ssg.streamingmurah.com:8286/currentsong?sid=1',
+          'https://cors-anywhere.herokuapp.com/https://ssg.streamingmurah.com:8286/currentsong',
         audioUrl: 'https://ssg.streamingmurah.com:8286/stream',
       },
       {
         title: 'RADIO IQ - SALURAN 2',
         audioInfo:
-          'https://cors-anywhere.herokuapp.com/https://ssg.streamingmurah.com:8256/currentsong?sid=1',
+          'https://cors-anywhere.herokuapp.com/https://ssg.streamingmurah.com:8256/currentsong',
         audioUrl: 'https://ssg.streamingmurah.com:8256/stream',
       },
       {
         title: 'RADIO IQ - SALURAN 3',
         audioInfo:
-          'https://cors-anywhere.herokuapp.com/https://ssg.streamingmurah.com:8126/currentsong?sid=1',
+          'https://cors-anywhere.herokuapp.com/https://ssg.streamingmurah.com:8126/currentsong',
         audioUrl: 'https://ssg.streamingmurah.com:8126/stream',
-      },
-      {
-        title: 'RADIO IQ - SALURAN 4',
-        audioInfo:
-          'https://cors-anywhere.herokuapp.com/https://ssg.streamingmurah.com:8134/currentsong?sid=1',
-        audioUrl: 'https://ssg.streamingmurah.com:8134/stream',
       },
     ],
     rss: 'https://anchor.fm/s/3770c8bc/podcast/rss',
