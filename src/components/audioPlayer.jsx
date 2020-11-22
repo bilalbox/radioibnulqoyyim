23f879437230d6a308c7ced7f15e506db2729c60
bilalbox <bilalbox@gmail.com>
2020-11-22 13:56:03 +0800

    remove player bling

--- src/components/audioPlayer.jsx
+++ src/components/audioPlayer.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { VimePlayer, VimeAudio, VimeDefaultUi } from '@vime/react'
-import './vime-player-dark.css'
-import './vime-player-light.css'
 
 export default function AudioPlayer({ audioSource }) {
   return (
